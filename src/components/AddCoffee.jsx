d9d8dea2619b288fdaa71cb06220ccfcd81da5c9
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-08 15:51:35 +0600

    initial project done

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const AddCoffee = () => {
+  return (
+    <div>
+      <h2>add coffee</h2>
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <span>Name</span>
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <span>Quantity</span>
+              <input
+                type="text"
+                placeholder="Available Quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
